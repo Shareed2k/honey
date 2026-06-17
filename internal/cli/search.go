96b5ef47f9ec4adbd6320dca95225e4d3ded9fd8
Roman Kredentser <roman@bringg.com>
2026-06-17 19:24:21 +0300

    cli: inline buildProviders and remove shallow wrapper

--- internal/cli/search.go
+++ internal/cli/search.go
@@ -106,7 +106,7 @@ func runSearchCore(cmd *cobra.Command, queryArgs []string) ([]hosts.Record, stri
 		}
 	}
 
-	provs := buildProviders(cfg, cmd)
+	provs := getSearchRegistry().BuildProviders(cfg, nil)
 	wantBackends := hosts.ParseBackendNames(flagBackends)
 	if len(wantBackends) > 0 {
 		provs = hosts.FilterBackendsByNames(provs, wantBackends)
