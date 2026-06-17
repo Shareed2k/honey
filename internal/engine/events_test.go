ea5fdd9169e271ae4562ac406213c50b3e602dc9
Roman Kredentser <roman@bringg.com>
2026-06-17 13:55:13 +0300

    docs: add design spec for recipe engine extraction

---
+++ internal/engine/events_test.go
@@ -0,0 +1,14 @@
+package engine_test
+
+import (
+	"testing"
+
+	"github.com/shareed2k/honey/internal/engine"
+)
+
+func TestEventInterfaces(t *testing.T) {
+	var e engine.Event = engine.EventStepStarted{StepIdx: 1}
+	if e.Kind() != engine.EventKindStepStarted {
+		t.Fatal("wrong kind")
+	}
+}
